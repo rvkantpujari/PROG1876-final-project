dbcf84b80e6de92825bb5884227c9e195c0fdd33
Ravi Kant Pujari <36390038+rvkantpujari@users.noreply.github.com>
2023-08-06 14:49:37 -0400

    add: insert query

--- DB_config.php
+++ DB_config.php
@@ -1,7 +1,7 @@
 <?php
     class DB
     {
-        // Configuration to interact with DB
+        // Config values to interact with DB
         const DB_USER = 'root';
         const DB_PASSWORD = '';
         const DB_HOST = 'localhost'; // MySQL runs on PORT 3308 instead of default port number 3306
@@ -15,6 +15,7 @@
         public $table;
         public $select = [];
 
+        // To configure the DB connection
         function __construct() {
             $this->dbc = @mysqli_connect(
                 self::DB_HOST,
@@ -29,30 +30,31 @@
             mysqli_set_charset($this->dbc, 'utf8');
         }
 
+        // To prepare string value
         function prepare_string($string) {
             $string = strip_tags($string);
             $string = mysqli_real_escape_string($this->dbc, trim($string));
             return $string;
         }
 
-        // get DB Connection String;
+        // To get DB Connection String
         function get_dbc() {
             return $this->dbc;
         }
 
-        // function set table name
+        // To set table name
         function table($table) {
             $this->table = $table;
             return $this;
         }
 
-        // function to select the columns
+        // To select all or specified columns
         function select($select = ['*']) {
             $this->select = $select;
             return $this;
         }
 
-        // function to all records
+        // To get all records
         function getAll() {
             // reset dataset array
             $this->dataset = array();
@@ -65,7 +67,7 @@
             return $this -> dataset;
         }
 
-        // function to add WHERE clause
+        // To add WHERE clause
         function where($cols, $vals, $oper, $colsType, $logOper = []) {
             // reset dataset array
             $this->dataset = array();
@@ -83,6 +85,7 @@
                 
                 $this->select = implode(", ", $this->select);
 
+                // SELECT query
                 $this->sqlQuery = "SELECT $this->select FROM $this->table WHERE $whereClause";
 
                 // prepare string values
@@ -90,12 +93,22 @@
                     $colsType[$i] === 's' ? $vals[$i] = "%".$this->prepare_string($vals[$i])."%" : "";
                 }
 
-                $results = $this->get_custom_result($colsType, $vals);
+                // prepare query and bind parameters
+                $stmt = $this->get_custom_result($colsType, $vals);
 
+                // execute statement
+                if(!$stmt->execute()) 
+                    return false;
+
+                // get results post statement execution
+                $results = $stmt->get_result();
+
+                // prepare dataset from results
                 while($row = mysqli_fetch_array($results, MYSQLI_ASSOC)) {
                     array_push($this->dataset, $row);
                 }
 
+                // return dataset
                 return $this -> dataset;
             } 
             else {
@@ -103,16 +116,43 @@
             }
         }
 
+        // To Insert data
+        function insert($cols, $vals, $colsType) {
+            // check if columns' and values' count is equal or not
+            if(count($cols) !== count($vals)) {
+                return "Error: Unequal columns count in the query.";
+            } else {
+                // prepare SQL Query structure
+                $columns = implode(", ", $cols);
+                $values = array_fill(0, count($vals), '?');
+                $values = implode(", ", $values);
+
+                // INSERT query
+                $this->sqlQuery = "INSERT INTO $this->table ($columns) VALUES($values)";
+                
+                // prepare string values
+                for($i = 0; $i < strlen($colsType); $i++) {
+                    $colsType[$i] == 's' ?  $vals[$i] = $this->prepare_string($vals[$i]) : "";
+                }
+
+                // prepare query and bind parameters
+                $stmt = $this->get_custom_result($colsType, $vals);
+
+                // execute statement
+                $result = mysqli_stmt_execute($stmt);
+
+                // return true if query executed successfully otherwise false
+                return $result;
+            }
+        }
+
+        // To prepare query and bind parameters
         function get_custom_result($types = null, $params = null) 
         {
+            // prepare query for execution
             $stmt = mysqli_prepare($this->dbc, $this->sqlQuery);
-
             $stmt->bind_param($types, ...$params);
-            
-            if(!$stmt->execute()) 
-                return false;
-                            
-            return $stmt->get_result();
+            return $stmt;
         }
         
         function __destruct() {
