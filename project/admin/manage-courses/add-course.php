474db45f4039d32dd5786c3309b2abe7e1b6cfee
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-08-17 20:26:51 -0400

    add: manage courses form validations

--- project/admin/manage-courses/add-course.php
+++ project/admin/manage-courses/add-course.php
@@ -16,6 +16,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $_SESSION['user']; ?> - Add Course | Enroll</title>
         <script src="https://cdn.tailwindcss.com"></script>
+        <style>
+            .error {color: red; font-size: 0.8rem;}
+        </style>
     </head>
     <body>
         <!-- Header/Navbar -->
@@ -38,30 +41,30 @@
                         </div>
 
                         <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-4 lg:p-12">
-                            <form class="grid grid-cols-8 gap-4">
+                            <form class="grid grid-cols-8 gap-4" id="addCourseForm">
                                 <div class="col-span-full md:col-span-2">
                                     <input type="hidden" name="add_course" id="add_course">
                                     <label class="text-sm text-gray-500" for="course_id">Course ID</label>
-                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Course ID" type="text" id="course_id"/>
+                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Course ID" type="text" name="course_id" id="course_id"/>
                                 </div>
 
                                 <div class="col-span-full md:col-span-4">
                                     <label class="text-sm text-gray-500" for="course_title">Course Title</label>
-                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Course Title" type="text" id="course_title"/>
+                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Course Title" type="text" name="course_title" id="course_title"/>
                                 </div>
 
                                 <div class="col-span-full md:col-span-2">
                                     <label class="text-sm text-gray-500" for="course_credits">Course Credits</label>
-                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Course Credits" type="text" id="course_credits"/>
+                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Course Credits" type="text" name="course_credits" id="course_credits"/>
                                 </div>
 
                                 <div class="col-span-full">
                                     <label class="text-sm text-gray-500" for="coruse_desc">Course Description</label>
-                                    <textarea class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Description" rows="5" id="course_desc"></textarea>
+                                    <textarea class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Description" rows="5" name="course_desc" id="course_desc"></textarea>
                                 </div>
 
-                                <div class="col-span-full mt-4 flex justify-between md:justify-start">
-                                    <button type="submit" class="btn inline-block rounded-lg bg-black mt-4 px-4 py-2 font-medium text-white sm:w-auto">Add Course</button>
+                                <div class="col-span-full flex justify-between md:justify-start">
+                                    <button class="btn inline-block rounded-lg bg-black mt-4 px-4 py-2 font-medium text-white sm:w-auto">Add Course</button>
                                     <a href="courses.php" class="inline-block rounded-lg bg-red-500 mt-4 ml-5 px-4 py-2 font-medium text-white sm:w-auto">Cancel</a>
                                 </div>
                             </form>
@@ -74,17 +77,45 @@
         <?php require ('../../layout/footer.php'); ?>
         <!-- Script -->
         <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.js"></script>
         <script>
             $(document).ready(function () 
             {
-                let btn = $('.btn').on('click', btnClicked);
+                // Form Validations
+                $( "#addCourseForm" ).validate({
+                    rules: {
+                        course_id: {
+                            required: true,
+                            minlength: 8,
+                            maxlength: 8,
+                        },
+                        course_title: {
+                            required: true,
+                            minlength: 8,
+                            maxlength: 50
+                        },
+                        course_credits: {
+                            min: 1,
+                            max: 4
+                        },
+                        course_desc: {
+                            required: true,
+                            minlength: 50,
+                            maxlength: 500
+                        }
+                    },
+                    submitHandler: function(form)
+                    {
+                        let btn = $('.btn').on('click', btnClicked);
+                    }
+                });
                 
                 function btnClicked(event) 
                 {
                     event.preventDefault();
-
+                    
                     let formData = { 
-                        add_course : $('#add_course').val(),
+                        add_course : true,
                         course_id : $('#course_id').val(), 
                         course_title : $('#course_title').val(),
                         course_credits : $('#course_credits').val(),
--- project/admin/manage-courses/edit-course.php
+++ project/admin/manage-courses/edit-course.php
@@ -25,6 +25,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $_SESSION['user']; ?> - Edit Course | Enroll</title>
         <script src="https://cdn.tailwindcss.com"></script>
+        <style>
+            .error {color: red; font-size: 0.8rem;}
+        </style>
     </head>
     <body>
         <!-- Header/Navbar -->
@@ -37,7 +40,7 @@
                         <div class="lg:col-span-2 lg:py-12">
                             <div class="mt-0">
                                 <span href="" class="text-2xl font-bold text-indigo-600">
-                                    Add Course
+                                    Edit Course
                                 </span>
 
                                 <div class="mt-2 not-italic">
@@ -47,25 +50,25 @@
                         </div>
 
                         <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-                            <form class="grid grid-cols-8 gap-4">
+                            <form class="grid grid-cols-8 gap-4" id="editCourseForm">
                                 <div class="col-span-full md:col-span-2">
                                     <label class="text-sm text-gray-500" for="course_id">Course ID</label>
-                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" value="<?php echo $record[0]['course_id']; ?>" placeholder="Course ID" type="text" id="course_id" readonly/>
+                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" value="<?php echo $record[0]['course_id']; ?>" placeholder="Course ID" type="text" name="course_id" id="course_id" readonly/>
                                 </div>
 
                                 <div class="col-span-full md:col-span-4">
                                     <label class="text-sm text-gray-500" for="course_title">Course Title</label>
-                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" value="<?php echo $record[0]['course_title']; ?>" placeholder="Course Title" type="text" id="course_title"/>
+                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" value="<?php echo $record[0]['course_title']; ?>" placeholder="Course Title" type="text" name="course_title" id="course_title"/>
                                 </div>
 
                                 <div class="col-span-full md:col-span-2">
                                     <label class="text-sm text-gray-500" for="course_credits">Course Credits</label>
-                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" value="<?php echo $record[0]['course_credits']; ?>" placeholder="Course Credits" type="text" id="course_credits"/>
+                                    <input class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" value="<?php echo $record[0]['course_credits']; ?>" placeholder="Course Credits" type="text" name="course_credits" id="course_credits"/>
                                 </div>
                                 
                                 <div class="col-span-full">
                                     <label class="text-sm text-gray-500" for="coruse_desc">Course Description</label>
-                                    <textarea class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Description" rows="8" id="course_desc"><?php echo $record[0]['course_desc']; ?></textarea>
+                                    <textarea class="w-full mt-2 rounded-lg border border-gray-300 p-3 text-md" placeholder="Description" rows="8" name="course_desc" id="course_desc"><?php echo $record[0]['course_desc']; ?></textarea>
                                 </div>
 
                                 <div class="mt-4 flex justify-between md:justify-start">
@@ -82,16 +85,13 @@
         <?php require ('../../layout/footer.php'); ?>
         <!-- Script -->
         <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.js"></script>
         <script>
             $(document).ready(function () 
-            {
-                let btn = $('.btn').on('click', btnClicked);
-                
+            {                
                 function btnClicked(event) 
                 {
                     event.preventDefault();
-                    
-                    // alert('clicked');
 
                     let formData = { 
                         update_course : true,
@@ -114,6 +114,35 @@
                         }
                     });
                 }
+
+                // Form Validations
+                $( "#editCourseForm" ).validate({
+                    rules: {
+                        course_id: {
+                            required: true,
+                            minlength: 8,
+                            maxlength: 8,
+                        },
+                        course_title: {
+                            required: true,
+                            minlength: 8,
+                            maxlength: 50
+                        },
+                        course_credits: {
+                            min: 1,
+                            max: 4
+                        },
+                        course_desc: {
+                            required: true,
+                            minlength: 50,
+                            maxlength: 500
+                        }
+                    },
+                    submitHandler: function(form)
+                    {
+                        let btn = $('.btn').on('click', btnClicked);
+                    }
+                });
             });
         </script>
     </body>
